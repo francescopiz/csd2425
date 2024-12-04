fc0782192797f8dda7ba10ce57762cec7d1316c0
atomicSteiner <damiano.buzzo@studenti.unicam.it>
2024-12-04 17:14:22 +0100

    [add] models and tour_service.dart (half done)

--- frontend/lib/services/tour_service.dart
+++ frontend/lib/services/tour_service.dart
@@ -1,6 +1,8 @@
 
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+import '../api/Poi.dart';
 import '../api/Tour.dart';
 
 class tour_service{
@@ -9,7 +11,7 @@ class tour_service{
   static String controllerPoi = 'api/v1/Poi';
   Future<List<Tour>> getAllTours() async {
     return http
-        .get(Uri.http(url, "$controller/GetAllTours"))
+        .get(Uri.http(url, "$controllerTour/GetAllTours"))
         .then((response) {
       if (response.statusCode == 200) {
         List<Tour> tours = [];
@@ -24,6 +26,19 @@ class tour_service{
     });
   }
   Future <List<Poi>> getAllPois() async {
-
+    return http
+        .get(Uri.http(url, "$controllerPoi/GetAllPois"))
+        .then((response) {
+      if (response.statusCode == 200) {
+        List<Poi> pois = [];
+        jsonDecode(Utf8Decoder().convert(response.bodyBytes))
+            .forEach((element) {
+          pois.add(Poi.fromJson(element));
+        });
+        return pois;
+      } else {
+        throw Exception('Failed to load pois');
+      }
+    });
   }
 }
