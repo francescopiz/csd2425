0856e6d7d81223ba4b08fc37f8070ad93382f2f7
KevinConcettoni <kevin.concettoni@hotmail.com>
2024-12-04 11:30:31 +0100

    [edit] Managed many-to-many relation between Poi and Tour

--- Landsight/Model/Configurations/PoiTourConfiguration.cs
+++ Landsight/Model/Configurations/PoiTourConfiguration.cs
@@ -8,12 +8,12 @@ namespace Landsight.Model.Configurations
     {
         public void Configure(EntityTypeBuilder<PoiTour> builder)
         {
-            builder.ToTable("PoiTour");
+            builder.ToTable("PoiTours");
             builder.HasKey(pt => new { pt.PoiId, pt.TourId });
-            builder.HasOne(pt => pt.Tour)
+            builder.HasOne(pt => pt.Poi)
                 .WithMany(p => p.PoiTours)
                 .HasForeignKey(pt => pt.PoiId);
-            builder.HasOne(pt => pt.Poi)
+            builder.HasOne(pt => pt.Tour)
                 .WithMany(t => t.PoiTours)
                 .HasForeignKey(pt => pt.TourId);
         }
