e224945aa8abe1c37d85f7ee6dad166e0ab06ce6
francesco <francy.pizzuto@gmail.com>
2024-12-22 14:43:58 +0100

    [edit] poi_details.dart to add next/previous poi button

--- frontend/lib/ui/poi_details.dart
+++ frontend/lib/ui/poi_details.dart
@@ -14,7 +14,7 @@ class PoiDetails extends StatefulWidget {
 class _PoiDetailsState extends State<PoiDetails> {
   late PageController _pageController;
   late int _currentPoiIndex;
-  int _currentPage = 0;
+  int _currentImage = 0;
 
   @override
   void initState() {
@@ -29,23 +29,19 @@ class _PoiDetailsState extends State<PoiDetails> {
     super.dispose();
   }
 
-  void _nextPage() {
-    if (_currentPage < widget.pois[_currentPoiIndex].mediafiles.length - 1) {
-      _pageController.nextPage(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-      setState(() => _currentPage++);
+  void _nextImage() {
+    if (_currentImage < widget.pois[_currentPoiIndex].mediafiles.length - 1) {
+      setState(() {
+        _currentImage++;
+      });
     }
   }
 
-  void _previousPage() {
-    if (_currentPage > 0) {
-      _pageController.previousPage(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-      setState(() => _currentPage--);
+  void _previousImage() {
+    if (_currentImage > 0) {
+      setState(() {
+        _currentImage--;
+      });
     }
   }
 
@@ -53,7 +49,8 @@ class _PoiDetailsState extends State<PoiDetails> {
     if (_currentPoiIndex < widget.pois.length - 1) {
       setState(() {
         _currentPoiIndex++;
-        _currentPage = 0;
+        //resetto l'indice dell'immagine
+        _currentImage = 0;
         _pageController.jumpToPage(_currentPoiIndex);
       });
     }
@@ -63,7 +60,8 @@ class _PoiDetailsState extends State<PoiDetails> {
     if (_currentPoiIndex > 0) {
       setState(() {
         _currentPoiIndex--;
-        _currentPage = 0;
+        //resetto l'indice dell'immagine
+        _currentImage = 0;
         _pageController.jumpToPage(_currentPoiIndex);
       });
     }
@@ -95,7 +93,7 @@ class _PoiDetailsState extends State<PoiDetails> {
                   top: MediaQuery.of(context).size.height * 0.15 - 24,
                   child: IconButton(
                     icon: const Icon(Icons.arrow_back_ios),
-                    onPressed: _previousPage,
+                    onPressed: _previousImage,
                   ),
                 ),
                 Positioned(
@@ -103,7 +101,7 @@ class _PoiDetailsState extends State<PoiDetails> {
                   top: MediaQuery.of(context).size.height * 0.15 - 24,
                   child: IconButton(
                     icon: const Icon(Icons.arrow_forward_ios),
-                    onPressed: _nextPage,
+                    onPressed: _nextImage,
                   ),
                 ),
               ],
